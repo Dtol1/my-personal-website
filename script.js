cb37523078bb06ccb98d3e4d1c2ad2380694db67
Davis Toler <tolerdavis0@gmail.com>
2022-08-01 14:17:40 -0500

    Update ideas

--- script.js
+++ script.js
@@ -1,4 +1,5 @@
 const h2 = document.createElement("h2");
-h2.textContent = "This content added by JavaScript";
+h2.textContent = "Learning how to coffee...I mean code";
 
 document.querySelector("body").appendChild(h2);
+
