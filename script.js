5ad3230c3ed240fb9fa0b7b59ced5272e61ede67
Davis Toler <tolerdavis0@gmail.com>
2022-08-01 16:14:38 -0500

    Added an event and images

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ h2.textContent = "Learning how to coffee...I mean code";
 
 document.querySelector("body").appendChild(h2);
 
+function showImage2() {
+    document.getElementById("image").src= "images/javaMug.jpg"
+}
+
+function showImage1() {
+    document.getElementById("image").src= "images/coffee-cup.jpg"
+}
